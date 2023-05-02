20061d580fee1c0b2bad6c38958e84784266d6af
Lixun Zhang <lixun.zhang@amd.com>
2023-05-02 13:33:45 -0500

    Enable multi-gpu tuning in batch_prof.sh

--- python/batch_prof.sh
+++ python/batch_prof.sh
@@ -1,35 +1,74 @@
 #! /bin/bash
 
-## $1: config file
-##     Each line in the config file must be dims for N, M, and K
-##     separated by space
-## $2: reduced tuning space
-## $3: output file
+##
+## Usage:
+##   ./batchprof.sh CONFIG_FILE OUTPUT
+## This script runs the tuning script for each gemm config in CONFIG_FILE.
+## Configs will be splitted on multiple GPUs on the system.
+## The results are collected in OUTPUT. If OUTPUT is not provided,
+## triton_prof_results.txt will be used
+##
+## This script is copied and modified from 20221122_Mathews_MIOpen_tuning.pdf.
 
-if [[ $# -lt 2 ]];then
-    echo "Usage: ./batch_prof.sh <config_file> <reducedTuningSpace>"
+set -e
+set -x
+
+## $1: config file
+##     Each line in the config file must be dims for M, N, and K
+##     separated by space
+## $2: output file
+
+PrWD=$(pwd)
+
+if [[ $# -lt 1 ]];then
+    echo "Usage: ./batch_prof.sh <config_file>"
     exit
 fi
 
-OUTPUT=$3
+CONFIG_FILE=$1
+OUTPUT=$2
 if [[ $OUTPUT == "" ]];then
-    OUTPUT="triton_prof_results.txt"
+    OUTPUT="${PrWD}/triton_prof_results.txt"
 fi
 
+DRIVER="${PrWD}/test/unit/language/matmul.py"
 
-DRIVER="./test/unit/language/matmul.py"
+DATESTR=$(date +'%Y%m%d_%H%M%S')
+RANDSTR=$RANDOM
 
-CONFIG_FILE=$1
-reduceSpace=$2
+mkdir triton_tuning_${DATESTR}_${RANDSTR}
+pushd triton_tuning_${DATESTR}_${RANDSTR}
 
-while IFS= read -r line
+shuf $PrWD/$CONFIG_FILE &> unique-configs-shuffled.log
+
+NGPUS=$(rocm-smi --showgpu | grep -Eo "GPU\[[0-9]*\]" | sed -e 's/\]//g' | wc -l)
+
+split -n l/$NGPUS unique-configs-shuffled.log configs_split
+split_files=($(ls configs_split*))
+
+for (( gpu=0; gpu<$NGPUS; gpu++ ));
 do
-    echo "config: $line"
-    gemm_config=($line)
-    N=${gemm_config[0]}
-    M=${gemm_config[1]}
-    K=${gemm_config[2]}
-    results=$(./tritonProfiler.sh $DRIVER $M $N $K $reduceSpace)
-    bestPerf=${results##*$'\n'}
-    echo "$bestPerf" | tee -a $OUTPUT
-done < "$CONFIG_FILE"
+    ## Add the profiler and driver program
+    sed -i "s#^#${PrWD}/tritonProfiler.sh $DRIVER #"  ${split_files[$gpu]}
+    ## Use the whole tuning space
+    sed -i "s/$/ 0/"  ${split_files[$gpu]}
+    sed -i "1s/^/export ROCM_VISIBLE_DEVICES=$gpu\n/" ${split_files[$gpu]}
+done
+wait
+
+# launch the training tasks concurrently
+for (( gpu=0; gpu<$NGPUS; gpu++ ));
+do
+    bash ${split_files[$gpu]} &> ${split_files[$gpu]}.log &
+done
+wait
+
+# collect results
+for (( gpu=0; gpu<$NGPUS; gpu++ ));
+do
+    sed -n "/Best Result/p" ${split_files[$gpu]}.log | tee -a $OUTPUT
+done
+
+popd
+
+echo "Tuned gemm configs are saved in: $OUTPUT"

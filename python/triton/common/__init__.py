95350db6c6e176b4e96d0c1ae0cbfb2ae965c785
Christian Sigg <csigg@google.com>
2023-07-10 11:15:03 +0200

    Remove python/, add BUILD files.

--- python/triton/common/__init__.py
+++
@@ -1,3 +0,0 @@
-from .build import _build
-
-__all__ = ["_build"]
--- python/triton/compiler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .compiler import CompiledKernel, compile
-from .errors import CompilationError
-
-__all__ = ["compile", "CompiledKernel", "CompilationError"]

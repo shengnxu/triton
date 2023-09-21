81280d6aab61c0dff29d892a72bfb9d01f8e01d0
Michael Melesse <micmelesse@gmail.com>
2023-09-21 12:00:17 -0500

    clean up build script

--- scripts/amd/build.sh
+++ scripts/amd/build.sh
@@ -1,15 +1,9 @@
 set -x
 
-cd python
+# bash scripts/amd/clean.sh
+
 pip uninstall -y triton
 
-bash scripts/amd/clean.sh
-
-export MLIR_ENABLE_DUMP=1
-export LLVM_IR_ENABLE_DUMP=1
-export AMDGCN_ENABLE_DUMP=1
-
 export TRITON_USE_ROCM=ON
-
-# pip install -U matplotlib pandas filelock tabulate
+cd python
 pip install --verbose -e .

948bb90fce4db62e8d115d1d094f7f59be3ca141
Lixun Zhang <lixun.zhang@amd.com>
2023-09-30 19:54:48 -0500

    Optimize when GROUP_M=1

--- scripts/amd/gemm/matmul.py
+++ scripts/amd/gemm/matmul.py
@@ -33,12 +33,16 @@ def prune_configs(configs, named_args):
         BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K =\
             kw["BLOCK_SIZE_M"], kw["BLOCK_SIZE_N"], kw["BLOCK_SIZE_K"]
         SPLIT_K = kw["SPLIT_K"]
+        GROUP_M = kw["GROUP_SIZE_M"]
         if SIZE_M <=32 and BLOCK_SIZE_M != 32:
             continue
         if SIZE_N <=32 and BLOCK_SIZE_N != 32:
             continue
         # skip large split_k when not necessary
         if SPLIT_K != 1 and not need_split_k(SIZE_M, SIZE_N, SIZE_K):
+            continue
+        # skip large GROUP_M
+        if GROUP_M * BLOCK_SIZE_M > SIZE_M:
             continue
         pruned_configs.append(config)
 
@@ -131,12 +135,16 @@ def matmul_kernel_splitK(
     pid_z = tl.program_id(1)
     num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
-    num_pid_in_group = GROUP_SIZE_M * num_pid_n
-    group_id = pid // num_pid_in_group
-    first_pid_m = group_id * GROUP_SIZE_M
-    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
-    pid_m = first_pid_m + (pid % group_size_m)
-    pid_n = (pid % num_pid_in_group) // group_size_m
+    if GROUP_SIZE_M == 1:
+        pid_m = pid // num_pid_n
+        pid_n = pid % num_pid_n
+    else:
+        num_pid_in_group = GROUP_SIZE_M * num_pid_n
+        group_id = pid // num_pid_in_group
+        first_pid_m = group_id * GROUP_SIZE_M
+        group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+        pid_m = first_pid_m + (pid % group_size_m)
+        pid_n = (pid % num_pid_in_group) // group_size_m
 
     # ----------------------------------------------------------
     # Create pointers for the first blocks of A and B.

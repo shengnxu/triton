ef1a1775b7affac39d1e9400690cc8f4f11c0c9e
Lixun Zhang <lixun.zhang@amd.com>
2023-10-04 10:37:54 -0500

    Compile all kernels in parallel
    And some other refactors:
    1. remove configs that cause out of shared memory error
    2. handling mfma16 and mfma32 generically
    3. generate code that can compile all kernel in parallel
    4. Add try_config
    5. Add cleanup option
    6. User can also specify m, n, k instead of providing the file
    7. Make the output of the tuning script a one liner for each size

--- scripts/amd/gemm/tune_gemm.py
+++ scripts/amd/gemm/tune_gemm.py
@@ -2,8 +2,8 @@ import argparse
 import sys
 import yaml
 import os
+import glob
 import subprocess
-
 
 
 def get_full_tuning_space():
@@ -32,8 +32,10 @@ def get_full_tuning_space():
 
 
 def prune_configs(M, N, K, configs):
-
     pruned_configs = []
+
+    ## TODO: improve how we deal with mfma16 vs mfma32
+    ## after it becomes a tuning parameter
     mfma_type = os.getenv('MFMA_TYPE')
     if mfma_type == '16':
         mfma = 16
@@ -46,6 +48,8 @@ def prune_configs(M, N, K, configs):
         BLOCK_SIZE_K = config.get("BLOCK_SIZE_K")
         SPLIT_K = config.get("SPLIT_K")
         GROUP_M = config.get("GROUP_SIZE_M")
+        if BLOCK_SIZE_M < mfma or BLOCK_SIZE_N < mfma:
+            continue
         if M <= mfma and BLOCK_SIZE_M != mfma:
             continue
         if N <= mfma and BLOCK_SIZE_N != mfma:
@@ -61,6 +65,11 @@ def prune_configs(M, N, K, configs):
         # skip large GROUP_M
         if GROUP_M * BLOCK_SIZE_M > M and GROUP_M != 1:
             continue
+        ## out of shared memory resource
+        ## TODO: figure out why these configs use so much LDS
+        if BLOCK_SIZE_K == 128 and (BLOCK_SIZE_M == 128 or BLOCK_SIZE_N == 128):
+            continue
+
         pruned_configs.append(config)
 
     return pruned_configs
@@ -75,7 +84,7 @@ def run_bash_command(commandstring):
     return proc.stdout.splitlines()
 
 
-def construct_funcName_from_config(M, N, K, config):
+def gen_kernel_and_configStr_from_config(M, N, K, config):
     block_m = config.get('BLOCK_SIZE_M')
     block_n = config.get('BLOCK_SIZE_N')
     block_k = config.get('BLOCK_SIZE_K')
@@ -83,91 +92,114 @@ def construct_funcName_from_config(M, N, K, config):
     split_k = config.get('SPLIT_K')
     num_warps = config.get('num_warps')
     num_stages = config.get('num_stages')
-    funcName = f"matmul_M{M}N{N}K{K}BM{block_m}BN{block_n}BK{block_k}GM{group_m}SK{split_k}nW{num_warps}nS{num_stages}"
+    configStr = f"M{M}_N{N}_K{K}_BM{block_m}_BN{block_n}_BK{block_k}_GM{group_m}_SK{split_k}_nW{num_warps}_nS{num_stages}"
 
-    kernel_call_str = f"""    try:
-        {funcName}[grid](
-            a, b, c,
-            M, N, K,
-            a.stride(0), a.stride(1),
-            b.stride(0), b.stride(1),
-            c.stride(0), c.stride(1),
-            BLOCK_SIZE_M = {block_m},
-            BLOCK_SIZE_N = {block_n},
-            BLOCK_SIZE_K = {block_k},
-            GROUP_SIZE_M = {group_m},
-            SPLIT_K = {split_k},
-            num_warps = {num_warps},
-            num_stages = {num_stages}
-        )
+    matmul_def_str = f"""
+def matmul_{configStr}(a, b, c):
+    M, K = a.shape
+    K, N = b.shape
+    grid = triton.cdiv(M, {block_m}) * triton.cdiv(N, {block_n}), {split_k}
+    print(f'config: matmul_kernel_{configStr}')
+    matmul_kernel_{configStr}[grid](
+        a, b, c,
+        M, N, K,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        c.stride(0), c.stride(1),
+        BLOCK_SIZE_M = {block_m},
+        BLOCK_SIZE_N = {block_n},
+        BLOCK_SIZE_K = {block_k},
+        GROUP_SIZE_M = {group_m},
+        SPLIT_K = {split_k},
+        num_warps = {num_warps},
+        num_stages = {num_stages}
+    )
+    return c
+
+def try_config_{configStr}(M, N, K, dtype):
+    a = torch.randn((M, K), device='cuda', dtype=dtype)
+    b = torch.randn((K, N), device='cuda', dtype=dtype)
+    c = torch.zeros((M, N), device=a.device, dtype=a.dtype)
+    try:
+        matmul_{configStr}(a, b, c)
     except Exception:
-        pass
+        print(f'invalid config {configStr}')
 """
-    return funcName, kernel_call_str
+    return configStr, matmul_def_str
 
 ## Open a file generated_kernelMNK.py and generate
 ## 1. matmul kernels of all configs
 ## 2. wrapper function matmul to invoke all the generated kernels
-## 3. test_gemm to invoke matmul in a loop of 10 iterations
+## 3. Another wraper function try_config to invoke matmul function
+## 4. test_gemm to invoke
+##    4.1 run try_config in parallel
+##    4.2 matmul in a loop of 10 iterations
 def generate_kernel(M, N, K, configs):
     f_kernel = open(f'generated_kernel{M}{N}{K}.py', 'w')
-    ## imports string
+
+    ### write imports
     import_str = """import torch
 import triton
 import triton.language as tl
+import argparse
+import sys
+import multiprocessing
 """
     f_kernel.write(import_str + "\n")
 
-    ## test_gemm string
-    test_gemm_str = """def test_gemm(M, N, K, dtype):
-    a = torch.randn((M, K), device='cuda', dtype=dtype)
-    b = torch.randn((K, N), device='cuda', dtype=dtype)
-    # Allocates output.
-    c = torch.zeros((M, N), device=a.device, dtype=a.dtype)
-    for i in range(10):
-        d = matmul(a, b, c)
-    return d
-"""
-    ## matmul string
-    matmul_pre_str = """def matmul(a, b, c):
-    assert a.shape[1] == b.shape[0], "Incompatible dimensions"
-    assert a.is_contiguous(), "Matrix A must be contiguous"
-    assert b.is_contiguous(), "Matrix B must be contiguous"
-    M, K = a.shape
-    K, N = b.shape
-"""
-
-    ## write matmul_kernels in generated_kernelMNK.py
+    ### write definitions of matmul_kernel_xxx
+    ### and matmul_xxx and try_config
     with open("matmul.kernel") as file:
         matmul_kernel_code = file.read();
     for config in configs:
-        funcName, _ = construct_funcName_from_config(M, N, K, config)
+        configStr, matmul_def_str = gen_kernel_and_configStr_from_config(M, N, K, config)
         ## Copy the matmul_kernel with name replaced
-        matmul_kernel_config = matmul_kernel_code.replace("matmul_kernel", funcName)
+        matmul_kernel_config = matmul_kernel_code.replace("matmul_kernel", f"matmul_kernel_{configStr}")
         f_kernel.write(matmul_kernel_config + "\n\n")
+        f_kernel.write(matmul_def_str + "\n")
 
-    ## write test_gemm
-    f_kernel.write(test_gemm_str + "\n")
+    ### write test_gemm
+    # pre string
+    test_gemm_pre_str = """def test_gemm(M, N, K, dtype, num_threads):
+    thread_pool = multiprocessing.Pool(processes=num_threads)
+    a = torch.randn((M, K), device='cuda', dtype=dtype)
+    b = torch.randn((K, N), device='cuda', dtype=dtype)
+    c = torch.zeros((M, N), device=a.device, dtype=a.dtype)
+    task_args = (M, N, K, dtype)
+"""
+    f_kernel.write(test_gemm_pre_str + "\n")
 
-    ## write matmul function
-    f_kernel.write(matmul_pre_str + "\n")
-
+    # warm up call of all matmul functions in parallel
     for config in configs:
-        block_m = config.get('BLOCK_SIZE_M')
-        block_n = config.get('BLOCK_SIZE_N')
-        split_k = config.get('SPLIT_K')
-        grid_str = f'    grid = triton.cdiv(M, {block_m}) * triton.cdiv(N, {block_n}), {split_k}'
-        funcName, kernel_call_str = construct_funcName_from_config(M, N, K, config)
+        configStr, _ = gen_kernel_and_configStr_from_config(M, N, K, config)
+        task_str = f"    thread_pool.apply_async(try_config_{configStr}, args=task_args)\n"
+        f_kernel.write(task_str)
 
-        ## write the kernel
-        f_kernel.write(grid_str + "\n\n")
-        f_kernel.write(kernel_call_str + "\n")
+    # call all matmul_xxx functions
+    for config in configs:
+        configStr, _ = gen_kernel_and_configStr_from_config(M, N, K, config)
+        matmul_call_str = f"""
+    for i in range(10):
+        d = matmul_{configStr}(a, b, c)"""
+        f_kernel.write(matmul_call_str + "\n")
+    # post string
+    f_kernel.write("    return d\n")
 
-    f_kernel.write("    return c\n")
-
-    ## call test_gemm
-    test_gemm_call_str = f'test_gemm({M}, {N}, {K}, torch.float16)'
-    f_kernel.write(test_gemm_call_str)
+    ### def main and call test_gemm
+    def_main_str = """
+def main():
+    parser = argparse.ArgumentParser(
+        prog="tune a specific gemm size",
+        allow_abbrev=False,)
+    parser.add_argument("-n", type=int, default=1, help='number of threads')
+    args = parser.parse_args()
+    numThreads = args.n
+    """
+    test_gemm_call_str = f'test_gemm({M}, {N}, {K}, torch.float16, numThreads)'
+    f_kernel.write(def_main_str)
+    f_kernel.write(test_gemm_call_str + "\n\n")
+    f_kernel.write("""if __name__ == '__main__':
+    sys.exit(main())""")
     f_kernel.close()
 
 
@@ -176,18 +208,21 @@ def tune_gemm_config(M, N, K, configs):
     generate_kernel(M, N, K, configs)
 
     ## remove any compiled kernel in the cache
-    rm_cache_cmd = "rm -rf ~/.triton/cache"
-    run_bash_command(rm_cache_cmd)
+    run_bash_command("rm -rf ~/.triton/cache")
+
+    ## precompile the kernels in parallel
+    ## TODO: parameterize numThreads at this level
+    run_bash_command(f"python generated_kernel{M}{N}{K}.py -n 16")
 
     ## profile generated kernels
-    rocprof_cmd = f"rocprof --stats python generated_kernel{M}{N}{K}.py"
-    run_bash_command(rocprof_cmd)
+    run_bash_command(f"rocprof --stats python generated_kernel{M}{N}{K}.py")
 
     ## post process results.csv to get the best config and minTime
+    ## TODO: process the file in parallel
     minTime = 1024 * 1024 * 1024
     for config in configs:
-        funcName, _ = construct_funcName_from_config(M, N, K, config)
-        parse_result_cmd = f'sed -n \'/{funcName}/p\' results.csv | awk -F \',\' \'{{print $NF}}\' | tail -n1'
+        configStr, _ = gen_kernel_and_configStr_from_config(M, N, K, config)
+        parse_result_cmd = f'sed -n \'/matmul_kernel_{configStr}/p\' results.csv | awk -F \',\' \'{{print $NF}}\' | tail -n1'
         parsed_outputs = run_bash_command(parse_result_cmd)
         if parsed_outputs:
             min_us = int(parsed_outputs[0]) / 1000
@@ -206,35 +241,67 @@ def parse_args():
         allow_abbrev=False,
     )
 
+    parser.add_argument("-m", type=int, default=0)
+    parser.add_argument("-n", type=int, default=0)
+    parser.add_argument("-k", type=int, default=0)
     parser.add_argument("--gemm_size_file", type=str, default="", help='yaml file to indicate matrix size')
+    parser.add_argument("--clean", action='store_true', default=False, help='remove generated files')
     args = parser.parse_args()
 
     return args
 
+
 def main():
     args = parse_args()
     matrix_size_file = args.gemm_size_file
-    if matrix_size_file == "" or not os.path.isfile(matrix_size_file):
-        print(f"Matrix size file: {matrix_size_file} does not exist!")
-        sys.exit(1)
+    needClean = args.clean
+
     mnks = []
-    with open(matrix_size_file) as file:
-        matrix_sizes = yaml.safe_load(file)
+    ## TODO: make it more robust to get user input
+    if matrix_size_file == "" or not os.path.isfile(matrix_size_file):
+        M = args.m
+        N = args.n
+        K = args.k
+        mnks = [(M, N, K)]
+    else:
+        with open(matrix_size_file) as file:
+            matrix_sizes = yaml.safe_load(file)
+        for sizes in matrix_sizes:
+            M = sizes['M']
+            N = sizes['N']
+            K = sizes['K']
+            mnks.append((M, N, K))
 
     configs_full = get_full_tuning_space()
-    for sizes in matrix_sizes:
-        M = sizes['M']
-        N = sizes['N']
-        K = sizes['K']
-        mnks.append((M, N, K))
 
     for (M, N, K) in mnks:
+        ## Obtain a pruned tuning space according to gemm size
         pruned_configs = prune_configs(M, N, K, configs_full)
-        print(f"Tuning GEMM (M: {M}, N: {N}, K: {K}) with {len(pruned_configs)} configs")
+
+        size_str = f'SIZE: {M} {N} {K}'
+        print(f"{size_str} nConfigs: {len(pruned_configs)}", end=" ", flush=True)
+
+        ## The main tuning funtion for one gemm size
         minTime, bestConfig = tune_gemm_config(M, N, K, pruned_configs)
-        perf_flops = lambda us: 2 * M * N * K * 1e-12 / (us)
-        out_str = f'SIZE: {M} {N} {K} '
-        print(f'{out_str} TFLOPS: {perf_flops(minTime)} time(us): {minTime} {bestConfig}')
+
+        ## post processing the numbers
+        perf_tflops = lambda us: 2 * M * N * K * 1e-12 / (us)
+        tri_tflops = perf_tflops(minTime)
+        if tri_tflops < 0.0001:
+            formatted_tflops = "{:.3e}".format(tri_tflops)
+        else:
+            formatted_tflops = "{:.2f}".format(tri_tflops)
+        print(f'TFLOPS: {formatted_tflops} time(us): {minTime}', end=" ")
+
+        bestConfig_compact_str, _ = gen_kernel_and_configStr_from_config(M, N, K, bestConfig)
+        print(f'best_config: {bestConfig_compact_str}')
+
+        ## remove generated files if asked to
+        if needClean:
+            os.remove(f"generated_kernel{M}{N}{K}.py")
+            for f in glob.glob("results.*"):
+                os.remove(f)
+
 
 if __name__ == '__main__':
     sys.exit(main())

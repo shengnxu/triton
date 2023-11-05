c11b237cae18387aa96a23a62067d284661c59df
Shucai Xiao <shucai.xiao@amd.com>
2023-11-04 19:05:51 -0700

    fix type names

--- python/tutorials/03-matrix-multiplication.py
+++ python/tutorials/03-matrix-multiplication.py
@@ -342,6 +342,7 @@ tl_to_torch_types = {
     tl.bfloat16: torch.bfloat16,
     tl.float32: torch.float32,
     tl.int8: torch.int8,
+    tl.int32: torch.int32,
 }
 if TORCH_HAS_FP8E5B16:
     tl_to_torch_types[tl.float8e5b16] = torch.float8_e5m2fnuz
@@ -350,6 +351,7 @@ if TORCH_HAS_FP8E4B8:
 
 name_to_tl_types = {
     'int8': tl.int8,
+    'int32': tl.int32,
     'fp16': tl.float16,
     'fp32': tl.float32,
     'bf16': tl.bfloat16,

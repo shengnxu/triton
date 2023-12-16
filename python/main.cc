69cedbe1ea5438bcab6e7382f6f1c6f9bc536bd4
Phil Tillet <phil@openai.com>
2023-12-15 22:25:21 -0800

    progress

--- python/main.cc
+++ python/main.cc
@@ -1,11 +1,8 @@
 #include <pybind11/pybind11.h>
 
-void init_superblocking(pybind11::module &m);
-void init_torch_utils(pybind11::module &m);
-void init_triton(pybind11::module &m);
-void init_cutlass(pybind11::module &m);
+void init_tritonamd(pybind11::module &m);
 
-PYBIND11_MODULE(libtriton, m) {
-  m.doc() = "Python bindings to the C++ Triton API";
-  init_triton(m);
+PYBIND11_MODULE(libtriton_amd, m) {
+  m.doc() = "Triton AMD backend API";
+  init_tritonamd(m);
 }

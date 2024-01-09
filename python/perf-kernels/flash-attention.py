b756dfba34aecf2a46cb7542d5563ac6aa109cbe
Joseph Groenenboom <joseph.groenenboom@amd.com>
2024-01-09 22:00:31 +0000

    bwd_kernel_dk_dv done

--- python/perf-kernels/flash-attention.py
+++ python/perf-kernels/flash-attention.py
@@ -136,10 +136,7 @@ def _attn_fwd_inner(
         if STAGE == 2:
             mask = offs_m[:, None] >= (start_n + offs_n[None, :])
             qk = tl.where(mask, qk, float("-inf"))
-        if BLOCK_M == 1:
-            qk += tl.sum(tl.view(q, [BLOCK_DMODEL]) * tl.view(k, [BLOCK_DMODEL]))
-        else:
-            qk += tl.dot(q, k)
+        qk += tl.dot(q, k)
         if bias_ptr is not None:
             if padded_block:
                 if bias_ptr.type.element_ty.is_block():
@@ -188,10 +185,7 @@ def _attn_fwd_inner(
         l_i = l_i * alpha + l_ij
         # update m_i and l_i
         m_i = m_ij
-        if BLOCK_M == 1:
-            acc += tl.view(p.to(V_block_ptr.type.element_ty), [1]) * tl.view(v, [BLOCK_DMODEL])
-        else:
-            acc += tl.dot(p.to(V_block_ptr.type.element_ty), v)
+        acc += tl.dot(p.to(V_block_ptr.type.element_ty), v)
         V_block_ptr = tl.advance(V_block_ptr, (BLOCK_N, 0))
         K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_N))
         if bias_ptr is not None:
@@ -394,7 +388,7 @@ def _attn_bwd_preprocess(O, DO,  #
     # write-back
     tl.store(NewDO + off_m[:, None] * D_HEAD + off_n[None, :], do)
     tl.store(Delta + off_m, delta)
-
+z
 
 @triton.jit
 def _bwd_kernel_dk_dv(
@@ -405,111 +399,149 @@ def _bwd_kernel_dk_dv(
     stride_qz, stride_qh, stride_qm, stride_qk,
     stride_kz, stride_kh, stride_kn, stride_kk,
     stride_vz, stride_vh, stride_vk, stride_vn,
-    Z, H, N_CTX,
+    Z, H, seqlen_q, seqlen_k,
+    dropout_p,
+    philox_seed,
+    philox_offset_base,
     BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
     BLOCK_N: tl.constexpr,
+    CAUSAL: tl.constexpr,
+    ENABLE_DROPOUT: tl.constexpr
 ):
-    start_m = tl.program_id(0)
+    start_m = tl.program_id(0) * BLOCK_N
     off_hz = tl.program_id(1)
     # Q is consumed depending on block ID. Every block uses
     # previous block offset by BLOCK_M x D_HEAD.
     qvk_offset = off_hz * stride_qh
-    qdo_offset = qvk_offset + start_m * BLOCK_M * stride_qm
     # initialize offsets
-    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_m = start_m + tl.arange(0, BLOCK_M)
     offs_n = tl.arange(0, BLOCK_N)
     offs_d = tl.arange(0, BLOCK_DMODEL)
     # Initialize pointers to Q, K, V
+    q_offset = off_hz * stride_qh
     Q_block_ptr = tl.make_block_ptr(
-        base=Q + qdo_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
+        base=Q + q_offset,
+        shape=(seqlen_q, BLOCK_DMODEL),
         strides=(stride_qm, stride_qk),
         offsets=(0, 0),
-        block_shape=(BLOCK_N, BLOCK_DMODEL),
+        block_shape=(BLOCK_M, BLOCK_DMODEL),
         order=(1, 0)
     )
+    k_offset = off_hz * stride_kh
     K_block_ptr = tl.make_block_ptr(
-        base=K + qvk_offset,
-        shape=(BLOCK_DMODEL, N_CTX),
+        base=K + k_offset,
+        shape=(BLOCK_DMODEL, seqlen_k),
         strides=(stride_kk, stride_kn),
-        offsets=(0, start_m * BLOCK_M),
+        offsets=(0, start_m),
         block_shape=(BLOCK_DMODEL, BLOCK_N),
         order=(0, 1)
     )
+    v_offset = off_hz * stride_vh
     V_block_ptr = tl.make_block_ptr(
-        base=V + qvk_offset,
-        shape=(BLOCK_DMODEL, N_CTX),
+        base=V + v_offset,
+        shape=(BLOCK_DMODEL, seqlen_k),
         strides=(stride_vn, stride_vk),
-        offsets=(0, start_m * BLOCK_M),
+        offsets=(0, start_m),
         block_shape=(BLOCK_DMODEL, BLOCK_N),
         order=(0, 1)
     )
+    do_offset = q_offset
     DO_block_ptr = tl.make_block_ptr(
-        base=DO + qdo_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
+        base=DO + do_offset,
+        shape=(seqlen_q, BLOCK_DMODEL),
         strides=(stride_qm, stride_qk),
         offsets=(0, 0),
-        block_shape=(BLOCK_N, BLOCK_DMODEL),
+        block_shape=(BLOCK_M, BLOCK_DMODEL),
         order=(1, 0)
     )
     # pointer to row-wise quantities in value-like data
-    D_ptrs = D + off_hz * N_CTX
-    l_ptrs = L + off_hz * N_CTX
+    D_ptrs = D + off_hz * seqlen_q
+    l_ptrs = L + off_hz * seqlen_q
     qk_scale = sm_scale * 1.44269504
     # load k and v: they will stay in SRAM throughout
     k = tl.load(K_block_ptr)
-    k = (k * qk_scale).to(k.dtype)
+    k = (k * qk_scale).to(K_block_ptr.type.element_ty)
     v = tl.load(V_block_ptr)
-    dv = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
-    dk = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+    vt = tl.load(VT_block_ptr)
+    dv = tl.zeros([BLOCK_N, BLOCK_DMODEL], dtype=tl.float32)
+    dk = tl.zeros([BLOCK_N, BLOCK_DMODEL], dtype=tl.float32)
     # This lower loop bound is because of the causal mask. We create a lower triangular
     # result. The upper triangular is -inf (becomes 0 when we do e^x). As such, it can
     # be ignored in the GEMM.
-    lo = start_m * BLOCK_M
-    hi = N_CTX
-    # loop over q, do
-    for start_n in range(lo, hi, BLOCK_N):
+    lo = start_m if CAUSAL else 0
+    hi = seqlen_q
+    Q_block_ptr = tl.advance(Q_block_ptr, (lo, 0))
+    DO_block_ptr = tl.advance(DO_block_ptr, (lo, 0))
+    batch_philox_offset = philox_offset_base + off_hz * seqlen_q * seqlen_k
+    '''
+           K1   K2      (d)V      dO
+    Q1    qk11 qk12     (d)v1     dO1
+    Q2    qk21 qk22     (d)v2     dO2
+
+    QK: (seqlen_q, seqlen_k)
+    dO: (seqlen_q, hdim)
+    dV: (seqlen_k, hdim)
+
+    dV = (QK)^T dO
+
+    dV1 = qk11 dO1 + qk21 dO2 = q1 k1 dO1 + q2 k1 dO2
+    dV2 = qk12 dO1 + qk22 dO2 = q1 k2 dO1 + q2 k2 dO2
+                                ~~~~~ = 0
+    start_m: select k and dV
+    start_n: select q and dO
+    '''
+    # loop over q (seqlen_q, dhead), do (seqlen_q, d_head)
+    for start_n in range(lo, hi, BLOCK_M):
         offs_m_curr = offs_n[:, None] + start_n
         # -- load q, do --
         q = tl.load(Q_block_ptr)
         do = tl.load(DO_block_ptr)
         # -- compute qk ----
         qk = tl.dot(q, k)
-        qk = tl.where(offs_m_curr >= offs_m[None, :], qk, float("-inf"))
+        if CAUSAL:
+            qk = tl.where(offs_m_curr >= offs_m[None, :], qk, float("-inf"))
         l_i = tl.load(l_ptrs + offs_m_curr)
         p = tl.math.exp2(qk - l_i)
         # -- compute dv ----
-        dv += tl.dot(tl.trans(p.to(do.dtype)), do)
+        if ENABLE_DROPOUT:
+            philox_offset = batch_philox_offset + start_n * seqlen_k + start_m
+            keep = dropout_mask(philox_seed, philox_offset, dropout_p, BLOCK_M, BLOCK_N, seqlen_k)
+            # CAVEAT: do NOT update p, ds needs the original p
+            dv += tl.dot(tl.where(tl.trans(keep), tl.trans(p) / (1 - dropout_p), 0.0).to(Q.dtype.element_ty), do)
+        else:
+            dv += tl.dot(tl.trans(p.to(do.dtype)), do)
         # compute dp = dot(v, do)
         Di = tl.load(D_ptrs + offs_m_curr)
-        dp = tl.zeros([BLOCK_N, BLOCK_M], dtype=tl.float32) - Di
-        dp += tl.dot(do, v)
+        dp = tl.zeros([BLOCK_M, BLOCK_M], dtype=tl.float32)
+        dp += tl.dot(do, vt)
+        if ENABLE_DROPOUT:
+            dp = tl.where(keep, dp / (1 - dropout_p), 0)
         # compute ds = p * (dp - delta[:, None])
-        ds = p * dp
+        ds = p * (dp - Di)
         # compute dk
         dk += tl.dot(tl.trans(ds.to(Q.dtype.element_ty)), q)
         # update pointers
-        Q_block_ptr = tl.advance(Q_block_ptr, (BLOCK_N, 0))
-        DO_block_ptr = tl.advance(DO_block_ptr, (BLOCK_N, 0))
+        Q_block_ptr = tl.advance(Q_block_ptr, (BLOCK_M, 0))
+        DO_block_ptr = tl.advance(DO_block_ptr, (BLOCK_M, 0))
     # initialize pointers to output
     DK_block_ptr = tl.make_block_ptr(
-        base=DK + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
+        base=DK + k_offset,
+        shape=(seqlen_k, BLOCK_DMODEL),
         strides=(stride_kn, stride_kk),
-        offsets=(start_m * BLOCK_M, 0),
+        offsets=(start_m, 0),
         block_shape=(BLOCK_M, BLOCK_DMODEL),
         order=(1, 0)
     )
     DV_block_ptr = tl.make_block_ptr(
-        base=DV + qvk_offset,
-        shape=(N_CTX, BLOCK_DMODEL),
+        base=DV + v_offset,
+        shape=(seqlen_k, BLOCK_DMODEL),
         strides=(stride_vk, stride_vn),
-        offsets=(start_m * BLOCK_M, 0),
+        offsets=(start_m, 0),
         block_shape=(BLOCK_M, BLOCK_DMODEL),
         order=(1, 0)
     )
     tl.store(DK_block_ptr, (dk * sm_scale).to(DK.dtype.element_ty))
-    tl.store(DV_block_ptr, dv.to(tl.float16))
+    tl.store(DV_block_ptr, dv.to(DK.type.element_ty))
 
 @triton.jit
 def _bwd_kernel_dq(

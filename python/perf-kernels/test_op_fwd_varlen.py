0fc9eb3ba7b8c71050525794210d92f3d7f2429f
Xiaohu Guo <xiaohu.guo@amd.com>
2024-01-23 10:19:52 -0800

    enable causal

--- python/perf-kernels/test_op_fwd_varlen.py
+++ python/perf-kernels/test_op_fwd_varlen.py
@@ -59,18 +59,19 @@ def test_op_fwd_varlen(Z, H, D_HEAD, causal,
     p= sm_scale * torch.einsum('qhd, khd -> qhk',q,k).half()
     print("p.shape=",p.shape)
 
-    start=0
-    for seq_len in seqlens_q:
+    if causal:
+        start=0
+        for seq_len in seqlens_q:
 
-        end=start+seq_len
+            end=start+seq_len
 
-        mask = torch.tril(torch.ones((seq_len, seq_len), device="cuda"))
-        if mask.size(0) != seq_len or mask.size(1) != seq_len:
-           raise ValueError(f"Mask shape {mask.size()} does not match sequence length {seq_len}")
-        expanded_mask = mask.unsqueeze(1).expand(seq_len, H, seq_len)
+            mask = torch.tril(torch.ones((seq_len, seq_len), device="cuda"))
+            if mask.size(0) != seq_len or mask.size(1) != seq_len:
+                raise ValueError(f"Mask shape {mask.size()} does not match sequence length {seq_len}")
+            expanded_mask = mask.unsqueeze(1).expand(seq_len, H, seq_len)
 
-        p[start:end, :, start:end] = p[start:end, :, start:end].masked_fill(expanded_mask == 0, float("-inf"))
-        start += seq_len
+            p[start:end, :, start:end] = p[start:end, :, start:end].masked_fill(expanded_mask == 0, float("-inf"))
+            start += seq_len
 
 #   if use_bias:
 #       # Add bias here as per the original implementation
@@ -78,6 +79,7 @@ def test_op_fwd_varlen(Z, H, D_HEAD, causal,
 
     p = torch.softmax(p, dim=-1)
     ref_out= torch.einsum('qhk, khd -> qhd',p,v).half()
+    print("ref_out= ", ref_out.shape)
 
     # Triton implementation (or other custom implementation)
 #    tri_out = attention(q, k, v, causal, bias, sm_scale)
@@ -90,7 +92,7 @@ H = 4  # Number of attention heads
 #max_seqlen_q = 10  # Maximum length of any sequence in the query batch
 #max_seqlen_k = 10  # Maximum length of any sequence in the key/value batch
 D_HEAD = 64  # Dimension of each head
-causal = False
+causal = True
 use_bias = False
 bias_type = "vector"
 

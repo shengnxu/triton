7812bd90a69073a6d1159ab90693ffb1175b2a77
Zoran Jovanovic <zjovanov@amd.com>
2024-03-05 14:43:05 +0000

    Fixed compilation error with missing CUtensorMapDataType.

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -26,21 +26,6 @@ using ::AMD::ConvertTritonGPUOpToLLVMPattern;
 
 
 namespace {
-static CUtensorMapDataType getCUtensorMapDataType(Type ty) {
-  if (ty.isF16()) {
-    return CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_FLOAT16;
-  } else if (ty.isBF16()) {
-    return CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_BFLOAT16;
-  } else if (ty.isF32()) {
-    return CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_FLOAT32;
-  } else if (ty.getIntOrFloatBitWidth() == 8) {
-    return CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_UINT8;
-  } else {
-    llvm::report_fatal_error("Unsupported elemTy for InsertSliceTMAOp");
-    return CUtensorMapDataType::CU_TENSOR_MAP_DATA_TYPE_FLOAT16;
-  }
-}
-
 // Contains some helper functions for both Load and Store conversions.
 struct LoadStoreConversionBase {
   explicit LoadStoreConversionBase(ModuleAxisInfoAnalysis &axisAnalysisPass)

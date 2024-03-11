8354cbc62deb17ec2ecd47b6df34d55124ebd384
Michael Melesse <micmelesse@gmail.com>
2024-03-11 12:52:58 -0500

    fix typo

--- python/perf-kernels/flash-attention.py
+++ python/perf-kernels/flash-attention.py
@@ -1235,7 +1235,7 @@ def run_benchmark(custom):
         line_names=[line_names],
         styles=[('red', '-')],
         ylabel='ms',
-        plot_name=f'fused-attention-varlen-{mode}-d{head-size}',
+        plot_name=f'fused-attention-varlen-{mode}-d{head_size}',
         args={
             'D_HEAD': head_size,
             'dtype': dtype,

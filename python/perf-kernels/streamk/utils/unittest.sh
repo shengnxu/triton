b7a99c0ca75bfbe85032a5ac9e15c5c435a3d74d
Xiaohu Guo <Xiaohu.Guo@amd.com>
2024-09-25 10:12:24 -0500

    add CI tests for streamk kernel

---
+++ python/perf-kernels/streamk/utils/unittest.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+TRITON_HIP_USE_NEW_STREAM_PIPELINE=1 python3 ./python/perf-kernels/streamk/tune_streamk.py --gemm_size_file ./python/perf-kernels/streamk/utils/streamk_unit_test_sizes.yaml --compare_wo_tuning

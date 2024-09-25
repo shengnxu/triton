4ff9faa7e3542feb7fd530d1a91582ed43bb0890
Xiaohu Guo <Xiaohu.Guo@amd.com>
2024-09-25 14:01:12 -0500

    more change to make git work

--- python/perf-kernels/streamk/utils/unittest.sh
+++ python/perf-kernels/streamk/utils/unittest.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-repo_root=$(git rev-parse --show-toplevel)
-git config --global --add safe.directory "$repo_root"
+git config --global --add safe.directory /__w/triton/triton
 TRITON_HIP_USE_NEW_STREAM_PIPELINE=1 python3 ./python/perf-kernels/streamk/tune_streamk.py --gemm_size_file ./python/perf-kernels/streamk/utils/streamk_unit_test_sizes.yaml --compare_wo_tuning

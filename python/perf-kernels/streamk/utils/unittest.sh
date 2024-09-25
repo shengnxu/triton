2755b725e7497cb67f6534b2d2a789736be0b49a
Xiaohu Guo <Xiaohu.Guo@amd.com>
2024-09-25 13:01:39 -0500

    fix git issue

--- python/perf-kernels/streamk/utils/unittest.sh
+++ python/perf-kernels/streamk/utils/unittest.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+repo_root=$(git rev-parse --show-toplevel)
+git config --global --add safe.directory "$repo_root"
 TRITON_HIP_USE_NEW_STREAM_PIPELINE=1 python3 ./python/perf-kernels/streamk/tune_streamk.py --gemm_size_file ./python/perf-kernels/streamk/utils/streamk_unit_test_sizes.yaml --compare_wo_tuning

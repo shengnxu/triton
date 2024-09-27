6abc43b982572c3cb01d8ff0687a4fd40f4c79fb
SJW <simon.waters@amd.com>
2024-09-27 21:15:21 +0000

    * working

--- third_party/amd/lib/TritonAMDGPUTransforms/StreamPipelineV2.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/StreamPipelineV2.cpp
@@ -192,8 +192,8 @@ void StreamPipeliner::createStreamCopy(
   assert(numStages >= 2 && "requires num_stages=2 at least");
   auto storeOp =
     builder.create<ttg::LocalStoreOp>(loc, copy->getResult(0), viewLoad);
-  schedule.insert(storeOp, localStoreStage, clusters[localStoreStage + 1]);
-  schedule.insert(viewLoad, localStoreStage, clusters[localStoreStage + 1]);
+  schedule.insert(storeOp, localStoreStage, clusters[localLoadStage + prefetch + 1]);
+  schedule.insert(viewLoad, localStoreStage, clusters[localLoadStage + prefetch + 1]);
 
   loadOp.erase();
 }
@@ -326,18 +326,14 @@ void StreamPipeliner::assignMemoryLayouts() {
         cast<tt::PointerType>(tensorTy.getElementType()).getPointeeType();
     unsigned width = vec * pointeeTy.getIntOrFloatBitWidth();
 
-    // Limit shared memory sharing to width >= 32 elements.
-    LDBG("Load " << *loadOp << " has width " << width);
-    if (width < 32) {
-      LDBG("Skip width<32 load " << *loadOp);
-      continue;
-    }
-
     if (use->hasTrait<OpTrait::DotLike>()) {
       // Only use shared memory when feeding into a dot op.
       loadInfo.usedByDot = true;
-      loadInfo.sharedEncoding =
-          getSharedEncIfAllUsersAreDotEnc(op->getResult(0)).value_or(nullptr);
+      // Limit shared memory sharing to width >= 32 elements.
+      if (width >= 32) {
+        loadInfo.sharedEncoding =
+            getSharedEncIfAllUsersAreDotEnc(op->getResult(0)).value_or(nullptr);
+      }
     } else if (auto useOp = dyn_cast<tt::LoadOp>(use)) {
       // The use of this loadOp is another loadOp. If the use is not in the
       // loadToInfo already, it means that the use is not valid for pipelining
